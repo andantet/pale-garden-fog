35b80e7fce5367a15f643315a86b9320cc2d59dc
Daniel Orr <orrdaniel73@gmail.com>
2024-10-02 16:32:35 +0100

    Initial commit

---
+++ src/client/kotlin/dev/andante/palegardenfog/PaleGardenFogClient.kt
@@ -0,0 +1,9 @@
+package dev.andante.palegardenfog
+
+import net.fabricmc.api.ClientModInitializer
+
+object PaleGardenFogClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
